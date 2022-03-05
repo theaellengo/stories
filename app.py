b1401f2e61989cc0eab37887d64f6ed632be02be
Thea Go <theaellengo@gmail.com>
2022-03-05 19:11:24 +0800

    init db

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-from consts import port, secret_key
+from consts import port, secret_key, dbname
+from db import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbname
 api = Api(app)
 app.secret_key = secret_key
 
@@ -12,4 +14,5 @@ app.secret_key = secret_key
 def index(): return 'Hello World'
 
 if __name__ == '__main__':
+  db.init_app(app)
   app.run(port=port, debug=True)
--- consts.py
+++ consts.py
@@ -1,2 +1,3 @@
 port = 5000
+dbname = 'stories_data.db'
 secret_key = 'q1er16sa5f7-fdfsa'
