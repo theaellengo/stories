5991619f1f9865a80d9c9f404017cf82e00019f2
Thea Go <theaellengo@gmail.com>
2022-03-07 14:43:53 +0800

    stories model

---
+++ models/stories.py
@@ -0,0 +1,37 @@
+from sqlalchemy import desc
+from db import db
+
+class StorieModel(db.Model):
+
+  __tablename__= 'stories'
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(80))
+  description = db.Column(db.String(300))
+
+  def __init__(self, title, description):
+    self.title = title
+    self.description = description
+
+  def json(self):
+    return {
+      'title': self.title,
+      'description': self.description
+    }
+
+  @classmethod
+  def find_by_name(self, name):
+    story = StorieModel.query.filter_by(name=name).first()
+    return story if story else None
+  
+  @classmethod
+  def find_by_id(self, _id):
+    story = StorieModel.query.filter_by(id=_id).first()
+    return story if story else None
+  
+  def save_to_db(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete_from_db(self):
+    db.session.delete(self)
+    db.session.commit()
