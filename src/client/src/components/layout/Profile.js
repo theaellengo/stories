62744967aa5cdfe0bc9e49d5e5bc2c04fe188500
Thea Go <theaellengo@gmail.com>
2022-03-12 20:36:26 +0800

    parts

--- src/client/src/components/layout/Profile.js
+++ src/client/src/components/layout/Profile.js
@@ -19,25 +19,12 @@ const Profile = () => {
 						culpa qui officia deserunt mollit anim id est laborum.
 					</div>
 				</div>
-				<div className="p-3 col-8">
-					<ul className="profile-choices">
-						<li>
-							<button className="btn btn-light">
-								<div className="title">Stories</div>
-							</button>
-						</li>
-						<li>
-							<button className="btn btn-light">
-								<div className="title">Parts</div>
-							</button>
-						</li>
-					</ul>
-
+				<div className="p-1 col-4">
+					<div className="title">Stories</div>
 					<div className="stories">
 						<div className="story bg-white p-2 m-2">
 							<h1 className="story-title">The Quick Brown Fox</h1>
 							<div>
-								<h4 className="story-owner">Username</h4>
 								<p>
 									Lorem ipsum dolor sit amet consectetur adipisicing elit.
 									Dignissimos placeat, dolorum ullam ipsam, sapiente suscipit
@@ -49,55 +36,70 @@ const Profile = () => {
 								</p>
 							</div>
 						</div>
-						<div className="story bg-white p-2 m-2">
-							<h1 className="story-title">The Quick Brown Fox</h1>
-							<div>
-								<h4 className="story-owner">Username</h4>
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Dignissimos placeat, dolorum ullam ipsam, sapiente suscipit
-									dicta eius velit amet aspernatur asperiores modi quidem
-									expedita fugit. Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Dignissimos placeat, dolorum ullam ipsam,
-									sapiente suscipit dicta eius velit amet aspernatur asperiores
-									modi quidem expedita fugit.
-								</p>
-							</div>
-						</div>
-
-						<div className="story-part px-2 m-2">
-							<p>username</p>
+					</div>
+					<div className="story bg-white p-2 m-2">
+						<h1 className="story-title">The Quick Brown Fox</h1>
+						<div>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-								posuere ante ut pretium lobortis. Proin convallis dolor massa,
-								nec congue metus imperdiet vitae. Pellentesque ut sagittis ante,
-								nec auctor magna. Nulla facilisi. Praesent ut ante in dolor
-								fringilla vehicula. Mauris finibus nec tellus eu tincidunt. Ut
-								id tincidunt magna, quis ornare mi. Nunc eget aliquam augue,
-								quis mattis libero. Maecenas et aliquam leo. Phasellus tincidunt
-								arcu id molestie vulputate. Ut facilisis ligula sit amet tellus
-								lobortis blandit. Pellentesque et augue a lorem aliquet
-								lobortis. Pellentesque ipsum ex, auctor et dignissim nec, semper
-								id tortor.
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Dignissimos placeat, dolorum ullam ipsam, sapiente suscipit
+								dicta eius velit amet aspernatur asperiores modi quidem expedita
+								fugit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Dignissimos placeat, dolorum ullam ipsam, sapiente suscipit
+								dicta eius velit amet aspernatur asperiores modi quidem expedita
+								fugit.
 							</p>
 						</div>
+					</div>
+				</div>
+				<div className="p-1 col-4">
+					<div className="title">Parts</div>
+					<div className="story-part pt-2">
+						<p>storyname</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+							posuere ante ut pretium lobortis. Proin convallis dolor massa, nec
+							congue metus imperdiet vitae. Pellentesque ut sagittis ante, nec
+							auctor magna. Nulla facilisi. Praesent ut ante in dolor fringilla
+							vehicula. Mauris finibus nec tellus eu tincidunt. Ut id tincidunt
+							magna, quis ornare mi. Nunc eget aliquam augue, quis mattis
+							libero. Maecenas et aliquam leo. Phasellus tincidunt arcu id
+							molestie vulputate. Ut facilisis ligula sit amet tellus lobortis
+							blandit. Pellentesque et augue a lorem aliquet lobortis.
+							Pellentesque ipsum ex, auctor et dignissim nec, semper id tortor.
+						</p>
+					</div>
 
-						<div className="story-part px-2 m-2">
-							<p>username</p>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-								posuere ante ut pretium lobortis. Proin convallis dolor massa,
-								nec congue metus imperdiet vitae. Pellentesque ut sagittis ante,
-								nec auctor magna. Nulla facilisi. Praesent ut ante in dolor
-								fringilla vehicula. Mauris finibus nec tellus eu tincidunt. Ut
-								id tincidunt magna, quis ornare mi. Nunc eget aliquam augue,
-								quis mattis libero. Maecenas et aliquam leo. Phasellus tincidunt
-								arcu id molestie vulputate. Ut facilisis ligula sit amet tellus
-								lobortis blandit. Pellentesque et augue a lorem aliquet
-								lobortis. Pellentesque ipsum ex, auctor et dignissim nec, semper
-								id tortor.
-							</p>
-						</div>
+					<div className="story-part pt-2">
+						<p>storyname</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+							posuere ante ut pretium lobortis. Proin convallis dolor massa, nec
+							congue metus imperdiet vitae. Pellentesque ut sagittis ante, nec
+							auctor magna. Nulla facilisi. Praesent ut ante in dolor fringilla
+							vehicula. Mauris finibus nec tellus eu tincidunt. Ut id tincidunt
+							magna, quis ornare mi. Nunc eget aliquam augue, quis mattis
+							libero. Maecenas et aliquam leo. Phasellus tincidunt arcu id
+							molestie vulputate. Ut facilisis ligula sit amet tellus lobortis
+							blandit. Pellentesque et augue a lorem aliquet lobortis.
+							Pellentesque ipsum ex, auctor et dignissim nec, semper id tortor.
+						</p>
+					</div>
+
+					<div className="story-part pt-2">
+						<p>storyname</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+							posuere ante ut pretium lobortis. Proin convallis dolor massa, nec
+							congue metus imperdiet vitae. Pellentesque ut sagittis ante, nec
+							auctor magna. Nulla facilisi. Praesent ut ante in dolor fringilla
+							vehicula. Mauris finibus nec tellus eu tincidunt. Ut id tincidunt
+							magna, quis ornare mi. Nunc eget aliquam augue, quis mattis
+							libero. Maecenas et aliquam leo. Phasellus tincidunt arcu id
+							molestie vulputate. Ut facilisis ligula sit amet tellus lobortis
+							blandit. Pellentesque et augue a lorem aliquet lobortis.
+							Pellentesque ipsum ex, auctor et dignissim nec, semper id tortor.
+						</p>
 					</div>
 				</div>
 			</div>
