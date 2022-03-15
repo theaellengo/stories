637f62e38031a008a5b3d4f55484c634d0c417fe
Thea Go <theaellengo@gmail.com>
2022-03-15 21:59:05 +0800

    load user, api not returning user data yet

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Stories from './components/layout/Stories';
@@ -7,6 +7,8 @@ import Login from './components/auth/Login';
 import Parts from './components/layout/Parts';
 import Profile from './components/layout/Profile';
 import Alert from './components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -14,24 +16,33 @@ import store from './store';
 
 import './App.css';
 
-const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Fragment>
-				<Navbar />
-				<div className="alert-container">
-					<Alert />
-				</div>
-				<Routes>
-					<Route exact path="/stories" element={<Stories />} />
-					<Route exact path="/register" element={<Register />} />
-					<Route exact path="/login" element={<Login />} />
-					<Route exact path="/part" element={<Parts />} />
-					<Route exact path="/profile" element={<Profile />} />
-				</Routes>
-			</Fragment>
-		</Router>
-	</Provider>
-);
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
+const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<div className="alert-container">
+						<Alert />
+					</div>
+					<Routes>
+						<Route exact path="/stories" element={<Stories />} />
+						<Route exact path="/register" element={<Register />} />
+						<Route exact path="/login" element={<Login />} />
+						<Route exact path="/part" element={<Parts />} />
+						<Route exact path="/profile" element={<Profile />} />
+					</Routes>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
--- src/client/src/actions/auth.js
+++ src/client/src/actions/auth.js
@@ -1,6 +1,26 @@
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+} from './types';
+
+export const loadUser = () => async (dispatch) => {
+	if (localStorage.token) {
+		setAuthToken(localStorage.token);
+	}
+
+	try {
+		const res = await axios.get(`/user`);
+
+		dispatch({ type: USER_LOADED, payload: res.data });
+	} catch (err) {
+		dispatch({ type: AUTH_ERROR });
+	}
+};
 
 export const register =
 	({ username, password, bio }) =>
